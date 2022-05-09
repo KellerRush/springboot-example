4873cb191cd5343f86ec1e71994a33d44be5c81f
lailx <lailx@baoxiniao.com>
2022-05-09 14:07:49 +0800

    init

---
+++ src/main/java/com/kellerrush/springbootexample/SpringBootExampleBootstrap.java
@@ -0,0 +1,13 @@
+package com.kellerrush.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleBootstrap.class, args);
+    }
+
+}
